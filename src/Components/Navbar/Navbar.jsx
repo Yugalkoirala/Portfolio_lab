ba940e2299a9f53fdcb79ba74c1aa6dc08db3fa8
yugal <yugal.koirala12@gmail.com>
2025-01-05 21:06:46 +0545

    cv added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
+import CVImage from "../../assets/CV.jpg";
+
 const Navbar = () => {
   const [menu, openMenu] = useState(false);
   const [showMenu, setShowmenu] = useState(true);
+
   return (
     <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
-      <span className="text-4xl font-bold tracking-wide text-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Portfolio</span>
+      <span className="text-4xl font-bold tracking-wide text-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+        Portfolio
+      </span>
 
       <ul
         className={`${
           menu ? "block" : "hidden"
-        }     mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
+        } mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
       >
         <a href="#About">
           <li className="text-md transition-all duration-300 p-1 md:p-0">
@@ -28,7 +33,16 @@ const Navbar = () => {
             Contact
           </li>
         </a>
+        <a
+          href={CVImage}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded transition-all duration-300"
+        >
+          Open CV
+        </a>
       </ul>
+
       {showMenu ? (
         <RiMenu2Line
           size={30}
@@ -42,6 +56,10 @@ const Navbar = () => {
         <RiCloseLine
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          onClick={() => {
+            openMenu(!menu);
+            setShowmenu(!showMenu);
+          }}
         />
       )}
     </nav>
