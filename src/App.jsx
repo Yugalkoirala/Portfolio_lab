29161b7dcf2805cd9be303e5358e6c06ab87bf74
yugal <yugal.koirala12@gmail.com>
2025-01-05 20:06:31 +0545

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import About from "./Components/About/About";
-// import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
@@ -11,7 +10,6 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      {/* <Experience /> */}
       <Projects />
       <Footer />
     </div>
